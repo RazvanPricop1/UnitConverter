5a31e9538878c38d0230e3365c0f65561e5fc0ff
Razvan Pricop <razvanipricop@gmail.com>
2024-09-30 16:25:42 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Razvan Pricop on 30.09.24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
